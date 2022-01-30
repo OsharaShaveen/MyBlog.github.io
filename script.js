678a2b9014c9a1e5012471e01561632143296a3f
NetworkChukka <89191830+NetworkChukka@users.noreply.github.com>
2022-01-30 16:03:58 +0530

    My Web

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Bot Developer", "Student", "A friend"],
+        strings: ["Anonymous Bot Developer", "Student", "A friend"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Bot Developer", "Student", "A friend"],
+        strings: ["Anonymous Bot Developer", "Student", "A friend"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
